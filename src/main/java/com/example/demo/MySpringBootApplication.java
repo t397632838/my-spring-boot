f9200972776e3be8cd77efc9646ede65e0bfa5a1
tangliang <397632838@qq.com>
2017-05-26 19:25:47 +0800

    tang
    commit

---
+++ src/main/java/com/example/demo/MySpringBootApplication.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vo.User;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+
+@SpringBootApplication
+@RestController
+public class MySpringBootApplication {
+
+	@RequestMapping("/")
+	public String name() {
+		return "Hello Spring" + this.count().size();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+	@Autowired
+	MongoOperations oper;
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+
+	public DBCursor count() {
+		DBCollection dbCollection = oper.getCollection("users");
+		User user = new User();
+		user.setName("沙瑞金");
+		this.mongoTemplate.save(user, "abc");
+
+		return dbCollection.find();
+	}
+
+}
---
+++ src/main/java/com/example/vo/User.java
@@ -0,0 +1,22 @@
+package com.example.vo;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.bson.BSONObject;
+
+import com.mongodb.DBObject;
+
+public class User {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
